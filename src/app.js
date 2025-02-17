58442668aba1ba2f4fd7a975f8393c4f08827d48
vikash <khowalvikash@gmail.com>
2025-02-17 22:24:24 +0530

    Multiple Route Handler

--- src/app.js
+++ src/app.js
@@ -5,15 +5,31 @@ const app = express();
 //     res.send("Test page")
 // })
 
-app.get('/test', (req, res) => {
-    res.send({fName: "vikash", lastName: "khowal"})
-})
-app.post('/test', (req, res) => {
-    res.send("Date Added Successfully!")
-})
-app.delete('/test', (req, res) => {
-    res.send("Data Deleted Successfully!")
-})
+// app.get('/test', (req, res) => {
+//     res.send({fName: "vikash", lastName: "khowal"})
+// })
+// app.post('/test', (req, res) => {
+//     res.send("Date Added Successfully!")
+// })
+// app.delete('/test', (req, res) => {
+//     res.send("Data Deleted Successfully!")
+// })
+
+app.use('/test', (req, res, next) => {
+    console.log("1st Handler");
+    // res.send("You are on 1st Route");
+    next()
+}, (req, res, next) => {
+    console.log("2nd Handler");
+    // res.send("You are on 2nd Route");
+    next()
+
+}, (req, res, next) => {
+    console.log("3rd Handler");
+    res.send("You are on 3rd Route");
+
+}
+)
 
 
 app.listen(5555, () => {
