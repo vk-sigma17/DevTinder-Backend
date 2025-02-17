e0837bb6ba66e817ae26e46417e4f24c3da80f71
vikash <khowalvikash@gmail.com>
2025-02-17 21:55:52 +0530

    Exploring Get, Post, Delete Requests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send("Test page")
-})
+// app.use('/test', (req, res) => {
+//     res.send("Test page")
+// })
 
-app.use('/main', (req, res) => {
-    res.send("Another Route")
+app.get('/test', (req, res) => {
+    res.send({fName: "vikash", lastName: "khowal"})
+})
+app.post('/test', (req, res) => {
+    res.send("Date Added Successfully!")
+})
+app.delete('/test', (req, res) => {
+    res.send("Data Deleted Successfully!")
 })
 
 
